644f18fb71ae21c908871eaa88b56467725ef1d1
crisvini <crisvini.leoncini@outlook.com>
2023-08-10 16:32:24 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: '/cristianleoncini.dev-react',
         element: <HomePage />
       },
       {
